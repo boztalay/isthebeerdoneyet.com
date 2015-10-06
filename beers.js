76b8d377352f2c60e579a27474186f877bfb6480
Ben Oztalay <boztalay@gmail.com>
2015-10-06 17:24:33 -0400

    Updating the American Wheat's listing

--- beers.js
+++ beers.js
@@ -4,7 +4,7 @@ beers = [
     "shortName": "AW",
     "done": false,
     "gone": false,
-    "state": "Fermenting",
+    "state": "Conditioning",
     "steps": [
       {
         "name": "Brew Day",
